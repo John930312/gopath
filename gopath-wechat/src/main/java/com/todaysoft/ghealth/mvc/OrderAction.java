4e9e530e8e2a1c2e5a49985994ac4e278f97d6f5
zhouyifan <zhouyifan@hsgene.com>
2018-11-13 15:21:27 +0800

    bug repair

--- gopath-wechat/src/main/java/com/todaysoft/ghealth/mvc/OrderAction.java
+++ gopath-wechat/src/main/java/com/todaysoft/ghealth/mvc/OrderAction.java
@@ -12,15 +12,20 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
@@ -117,4 +122,21 @@ public class OrderAction
         return "order/order_detail";
     }
 
+    @RequestMapping(value = "/getPDF", produces = MediaType.APPLICATION_PDF_VALUE)
+    @ResponseBody
+    public byte[] getPDF(@RequestParam(value = "path", required = false) String path) throws IOException
+    {
+        if (path != null)
+        {
+            File file = new File(path);
+            FileInputStream inputStream = new FileInputStream(file);
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes, 0, inputStream.available());
+            return bytes;
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
