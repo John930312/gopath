f976a7188b742eb253428a421edf17a49b85476f
John <ljl845851667@163.com>
2018-11-19 14:11:08 +0800

    bug

--- gopath-mgmt/src/main/java/com/todaysoft/ghealth/service/wrapper/CustomerWrapper.java
+++ gopath-mgmt/src/main/java/com/todaysoft/ghealth/service/wrapper/CustomerWrapper.java
@@ -2,7 +2,10 @@ package com.todaysoft.ghealth.service.wrapper;
 
 import com.todaysoft.ghealth.DTO.CustomerDTO;
 import com.todaysoft.ghealth.model.Customer;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
 
 /**
  * @Author: xjw
@@ -21,6 +24,10 @@ public class CustomerWrapper extends Wrapper<CustomerDTO, Customer>
     protected void setCopyIgnoreProperties(CustomerDTO source, Customer target)
     {
         target.setCreateTime(parseDate(source.getCreateTime()));
-        target.setBirthday(parseDate(source.getBirthday()));
+        try {
+            target.setBirthday(null == (source.getBirthday()) ? null : DateUtils.parseDate(source.getBirthday(), "yyyy-MM-dd"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
