0f2bfaa0aac8b63ff74c6ca10a75f9ab064fb10d
江苏-徐凯 <xukai@hsgene.com>
2018-11-19 10:13:47 +0800

    脚本更新

---
+++ dbscript/20181119.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `ghealth_gopath_sample_box` MODIFY `NAME` VARCHAR (64) DEFAULT NULL;
+ALTER TABLE `ghealth_gopath_sample_box` MODIFY `CODE` VARCHAR (64) DEFAULT NULL;
+ALTER TABLE `ghealth_gopath_order` MODIFY  `ORDER_URL` VARCHAR (256);
